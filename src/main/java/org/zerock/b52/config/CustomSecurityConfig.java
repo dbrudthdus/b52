50132344ba4f5cf16a34113e50b3515018862317
yk <dbrudthdus@gmail.com>
2023-06-23 18:30:41 +0900

    0623

--- src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.zerock.b52.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,19 +10,33 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+	private final DataSource dataSource;
 
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 
 		return new BCryptPasswordEncoder();
 	}
+
+	  @Bean
+  public PersistentTokenRepository persistentTokenRepository() {
+      JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+      repo.setDataSource(dataSource);
+      return repo;
+  }
+
 	
 	@Bean
 	public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
@@ -34,6 +50,11 @@ public class CustomSecurityConfig {
 		http.formLogin(config -> {
 			config.loginPage("/member/signin");
 		});
+
+		 http.rememberMe(config -> {
+        config.tokenRepository(persistentTokenRepository());
+        config.tokenValiditySeconds(60*60*24*7);
+      }); 
 
 		http.csrf(config -> {
 			config.disable();
